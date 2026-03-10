3a4383719d2d56869ba167a69f6e5f47a28751e2
sai_eswar_ <saieswar750@gmail.com>
2026-03-10 07:58:46 -0700

    made some changes in the file

--- dragonball.js
+++ dragonball.js
@@ -5,47 +5,53 @@ const dragonball = async () => {
   const response = await fetch("https://dragonball-api.com/api/characters");
   const data = await response.json();
   images = data.items;
-  console.log(data)
+  console.log(data);
   return images;
 };
 
 const imagesFetch = async () => {
   let dragonballfun = await dragonball();
-  console.log(images);
-  images.map((eachitem) => {
+  console.log(dragonballfun);
+
+  dragonballfun.map((eachitem) => {   // changed from images.map
+
     let childdiv = document.createElement("div");
     childdiv.classList.add("childdiv-styles");
 
-    //to get image
+    // image
     let imgEle = document.createElement("img");
     imgEle.src = eachitem.image;
     imgEle.classList.add("image-styles");
 
-    // to get names
+    // name
     let charnames = document.createElement("h1");
     charnames.textContent = eachitem.name;
     charnames.classList.add("namestyle");
-    // to get maxki
+
+    // max ki
     let Maxki = document.createElement("p");
     Maxki.textContent = eachitem.maxKi;
     Maxki.classList.add("paragraph-style");
-    //to get gender
-    let genderchar =document.createElement("p")
-    genderchar.textContent=eachitem.gender
+
+    // gender
+    let genderchar = document.createElement("p");
+    genderchar.textContent = eachitem.gender;
     genderchar.classList.add("paragraph-style");
-// to get race
-let racechar =document.createElement("p")
-racechar.textContent=eachitem.race
-racechar.classList.add("paragraph-style");
+
+    // race
+    let racechar = document.createElement("p");
+    racechar.textContent = eachitem.race;
+    racechar.classList.add("paragraph-style");
 
     childdiv.appendChild(imgEle);
     childdiv.appendChild(charnames);
     childdiv.appendChild(Maxki);
-    childdiv.appendChild(genderchar)
-    childdiv.appendChild( racechar)
+    childdiv.appendChild(genderchar);
+    childdiv.appendChild(racechar);
+
     container.appendChild(childdiv);
 
-    return dragonballfun;
   });
 };
+
 imagesFetch();
