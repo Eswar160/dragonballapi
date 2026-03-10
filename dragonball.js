b0092a3694659adeefe907edfb4acea34a62caeb
sai_eswar_ <saieswar750@gmail.com>
2026-03-10 07:22:37 -0700

    first commit

---
+++ dragonball.js
@@ -0,0 +1,51 @@
+const container = document.getElementById("container");
+let images = [];
+
+const dragonball = async () => {
+  const response = await fetch("https://dragonball-api.com/api/characters");
+  const data = await response.json();
+  images = data.items;
+  console.log(data)
+  return images;
+};
+
+const imagesFetch = async () => {
+  let dragonballfun = await dragonball();
+  console.log(images);
+  images.map((eachitem) => {
+    let childdiv = document.createElement("div");
+    childdiv.classList.add("childdiv-styles");
+
+    //to get image
+    let imgEle = document.createElement("img");
+    imgEle.src = eachitem.image;
+    imgEle.classList.add("image-styles");
+
+    // to get names
+    let charnames = document.createElement("h1");
+    charnames.textContent = eachitem.name;
+    charnames.classList.add("namestyle");
+    // to get maxki
+    let Maxki = document.createElement("p");
+    Maxki.textContent = eachitem.maxKi;
+    Maxki.classList.add("paragraph-style");
+    //to get gender
+    let genderchar =document.createElement("p")
+    genderchar.textContent=eachitem.gender
+    genderchar.classList.add("paragraph-style");
+// to get race
+let racechar =document.createElement("p")
+racechar.textContent=eachitem.race
+racechar.classList.add("paragraph-style");
+
+    childdiv.appendChild(imgEle);
+    childdiv.appendChild(charnames);
+    childdiv.appendChild(Maxki);
+    childdiv.appendChild(genderchar)
+    childdiv.appendChild( racechar)
+    container.appendChild(childdiv);
+
+    return dragonballfun;
+  });
+};
+imagesFetch();
